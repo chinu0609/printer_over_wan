f8051b231f194bc3f5e46088a3ef33efad87527c
Chinmay Bhosale <chinmayhbhosale02@gmail.com>
2024-01-09 22:00:07 +0530

    printer

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, redirect, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import FileField
+from werkzeug.utils import secure_filename
+
+
+
+app = Flask("Ganesha")
+app.config["SECRET_KEY"] = "ganu"
+
+
+class FileForm(FlaskForm):
+    file =  FileField(label="your file here")
+    
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/upload",methods =["GET","POST"])
+def upload():  
+    form = FileForm() 
+  
+    if form.validate_on_submit():
+
+      
+        filename = secure_filename(form.file.data.filename)
+        form.file.data.save('uploads/'+filename)
+        return redirect(url_for('upload'))
+    return render_template("index.html",form=form)
+   
+        
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
